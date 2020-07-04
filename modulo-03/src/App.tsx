7474a7a8d80bb1da3d4612c48bb26c2b63f4c5d9
Leonardo Braz <lhleonardo05@gmail.com>
2020-07-04 20:48:18 -0300

    estilo global com styled components

--- modulo-03/src/App.tsx
+++ modulo-03/src/App.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './styles/globalStyle';
 
-const App: React.FC = () => <BrowserRouter><Routes /></BrowserRouter>;
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
